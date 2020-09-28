32b71b67790f4bd2ef13dea7729976d1ff3d4605
Alejandro-Rojas <rojas.alejandro303@gmail.com>
2020-09-27 23:12:20 -0600

    read and write file

--- Symmetric_Message/src/reciever.java
+++ Symmetric_Message/src/reciever.java
@@ -1,3 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class reciever {
+    public static void main(String[] args) throws IOException {
+        String data = "This is the data in the output file";
+  
+      try {
+        // Creates a FileWriter
+        FileWriter file = new FileWriter("rKey.txt");
+  
+        // Creates a BufferedWriter
+        BufferedWriter output = new BufferedWriter(file);
+  
+        // Writes the string to the file
+        output.write(data);
+  
+        // Closes the writer
+        output.close();
+      }
+  
+      catch (Exception e) {
+        e.getStackTrace();
+      }
+    }
+  }
     
-}
+
--- Symmetric_Message/src/sender.java
+++ Symmetric_Message/src/sender.java
@@ -1,3 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+
 public class sender {
-    
+
+    public static void main(String[] args) throws IOException {
+      String data = "This is the data in the output file";
+
+    try {
+      // Creates a FileWriter
+      FileWriter file = new FileWriter("senderKey.txt");
+
+      // Creates a BufferedWriter
+      BufferedWriter output = new BufferedWriter(file);
+
+      // Writes the string to the file
+      output.write(data);
+
+      // Closes the writer
+      output.close();
+    }
+
+    catch (Exception e) {
+      e.getStackTrace();
+    }
+  }
 }
